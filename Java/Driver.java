618afb51da4f84a2b4bf2ef8083ff1ac51a15851
Sergio Sepulveda <segiomid@gmail.com>
2022-04-08 15:52:45 -0500

    Java: creación subclases Account

---
+++ Java/Driver.java
@@ -0,0 +1,6 @@
+public class Driver extends Account {
+    
+    public Driver(Integer id, String name, String document, String email, String password) {
+        super(id, name, document, email, password);        
+    }
+}
---
+++ Java/User.java
@@ -0,0 +1,7 @@
+public class User extends Account {
+
+    public User(Integer id, String name, String document, String email, String password) {
+        super(id, name, document, email, password);        
+    }
+    
+}
