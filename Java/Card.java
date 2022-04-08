2708223497e22aacc91bc0f29869dd8c34a13b61
Sergio Sepulveda <segiomid@gmail.com>
2022-04-08 15:27:25 -0500

    Java: creación clase y subclases Payment

---
+++ Java/Card.java
@@ -0,0 +1,14 @@
+public class Card extends Payment {
+    
+    Integer number;
+    Integer cvv;
+    String date;
+
+    public Card(Integer id, Integer number, Integer cvv, String date){
+        super(id);
+        this.number = number;
+        this.cvv = cvv;
+        this.date = date;
+
+    }
+}
---
+++ Java/Paypal.java
@@ -0,0 +1,11 @@
+public class Paypal extends Payment {
+
+    String email;
+
+    public Paypal(Integer id, String email) {
+        super(id);
+        this.email = email;
+        
+    }
+    
+}
