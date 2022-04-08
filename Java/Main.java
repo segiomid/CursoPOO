2775d600aef22a8f2996dc561b286ce9e96df642
Sergio Sepulveda <segiomid@gmail.com>
2022-04-08 15:52:27 -0500

    Java: creación clase y subclases Account

--- Java/Main.java
+++ Java/Main.java
@@ -1,11 +1,11 @@
 class Main{
     public static void main(String[] args){
         System.out.println("Hola Mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+        Car car = new Car("AMQ123", new Account(1,"Andres Herrera", "AND123","se@gmama.com","123suif"));
         car.passegenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        Car car2 = new Car("QWE567", new Account(2,"Andrea Herrera", "ANDA876","ddd@ffff.com","sadad23"));
         car2.passegenger = 3;
         car2.printDataCar();
     }
