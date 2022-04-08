c71814b57e8c32da755aec1632eaecb9fd9fe6a6
Sergio Sepulveda <segiomid@gmail.com>
2022-04-08 16:04:01 -0500

    Python: Clase y subclases account. Ajuste campos para inicializar Account en car

--- Python/car.py
+++ Python/car.py
@@ -1,12 +1,9 @@
 from account import Account
 
-
-from account import Account
-
 class Car:
     id = int
     license = str
-    driver = Account('','')
+    driver = Account('','','','','')
     passenger = str
 
     def __init__(self, license, driver):
--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,7 @@ from account import Account
 
 if __name__ == "__main__":
     print("Hola xxxxxx")
-    car = Car('AMQ123',Account('Andrés Herrera','1090222'))
-    car2 = Car('EIY594', Account('Sergio Sepúlveda','1010101'))
+    car = Car('AMQ123',Account(1,'Andrés Herrera','1090222','sss@g.com','23sds'))
+    car2 = Car('EIY594', Account(2,'Sergio Sepúlveda','1010101','sss@g.com','23sds'))
     print(vars(car2))
     print(vars(car2.driver))
